c5295f0bf3620a570278daac8bd7c14985f91514
pathikrit <pathikritbhowmick@msn.com>
2025-03-27 13:50:44 -0400

    also get the raw text

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-import asyncio
+from typing import List, ClassVar
 
 import streamlit as st
 
-from typing import List, ClassVar
-
 from pydantic import BaseModel, Field
 from litellm import image_generation, completion
+
 
 class ImageTag(BaseModel):
     id: int = Field(description="Image id starting from 1 - I will use this to replace the [[replace_image_X]] tags")
@@ -21,18 +20,21 @@ class ImageTag(BaseModel):
             size=f"{ImageTag.size}x{ImageTag.size}",
         )
 
+
 class Story(BaseModel):
-    html: str = Field(description="The story (in html that would go inside <body>)")
+    text: str = Field(description="Raw text of the story I will feed to a speech to text engine")
+    html: str = Field(description="The same story (in html that would go inside <body>) to be rendered prettily")
     images: List[ImageTag] = Field(description="The images for the story")
 
     @classmethod
     async def generate(cls, who: str, prompt: str, bedtime: bool):
         system_prompt = (
-            f"Generate a creative {'bedtime' if bedtime else 'and engaging'} story for {who}\n"            
+            f"Generate a creative {'bedtime' if bedtime else 'and engaging'} story for {who}\n"
             "Include the child in the story also\n"
-            "Return the story as html that would go inside the <body> tag\n"
-            "For styles, use inline styles on the HTML elements\n"
-            "Also include placeholder image tags (2-3) as follows:\n"
+            "Return the story as text (for audio transcription) and html that would go inside the <body> tag\n"
+            "Make sure the text inside the html is same as the raw text\n"
+            "For styles in the html, use inline styles on the HTML elements\n"
+            "Also include placeholder image tags (2-3) in the html as follows:\n"
             f"<img src='[[replace_image_1]]' width='{ImageTag.size}' height='{ImageTag.size}'/>"
             "Return these tags separately with a short prompt that I would use an AI to generate the images"
             "I will use the [[replace_image_X]] to replace with the image urls from image generation API separately"
@@ -53,11 +55,12 @@ class Story(BaseModel):
 
 
 if __name__ == "__main__":
+    st.set_page_config(layout="wide")
     st.title("Story Generator")
     who = st.text_input("Who:", "3-year old boy named Aidan")
     prompt = st.text_input("Prompt:", "colorful bison and a monster truck")
     bedtime = st.checkbox("Bedtime")
 
     if st.button("Generate Story"):
-        html = asyncio.run(Story.generate(who=who, prompt=prompt, bedtime=bedtime))
+        html = Story.generate(who=who, prompt=prompt, bedtime=bedtime)
         st.html(html)
