ebdb284843d3a14c683b940a3e3ffe46acad7489
pathikrit <pathikritbhowmick@msn.com>
2025-04-29 11:25:12 -0400

    move models to own file

--- server.py
+++ server.py
@@ -1,6 +1,4 @@
 import os
-from typing import List, ClassVar
-import re
 from concurrent.futures import ThreadPoolExecutor
 from functools import lru_cache
 from collections import deque
@@ -9,78 +7,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
-from pydantic import BaseModel, Field
-
-from ai import ask_ai
-
-
-class ImageTag(BaseModel):
-    id: int = Field(description="Image id starting from 1 - I will use this to replace the [[replace_image_X]] tags")
-    prompt: str = Field(description="\n".join([
-        "The prompt for the image that I will feed to the image generation API",
-        "Must be detailed pertaining to location in the story wrt to the image tag id"
-    ]))
-    size: ClassVar[int] = 1024
-
-    def generate(self):
-        image = ask_ai(
-            mode="image",
-            instructions=[
-                "Generate a children's storybook image for the given prompt",
-                "Also make sure, there are no texts in the generated image"
-            ],
-            prompt=self.prompt,
-            response_format="png",
-            background="transparent",
-            quality="medium",
-            size=f"{ImageTag.size}x{ImageTag.size}"
-        )
-        return self.id, image
-
-
-class Story(BaseModel):
-    file_name: str = Field(description="File name (without extension) to export this story to a html file")
-    title: str = Field(description="Title of the story")
-    html: str = Field(description="The story in HTML which would go inside a div (include the title)")
-    images: List[ImageTag] = Field(description="The images for the story")
-
-    @classmethod
-    def generate(cls, prompt: str, who: str, bedtime: bool, include_child: bool):
-        story = ask_ai(
-            mode="text",
-            instructions=[
-                f"Generate a imaginative and creative {'bedtime' if bedtime else 'and engaging'} story for {who}",
-                "Include the child in the story also" if include_child else "No need to include the child in the story",
-                "Return the story as html (which would go inside div)",
-                "Also include placeholder image tags (1-2) inside the text at appropriate locations follows:",
-                "<img src='[[replace_image_1]]' hidden>",
-                "Return these tags separately with a short prompt (appropriate for the section in the story) that I would use an AI to generate the images",
-                "Every image prompt must include detailed descriptions of the story characters to allow similar character generation by AI"
-                "I will use the [[replace_image_X]] to replace with the image urls from image generation API separately and unhide these images",
-                "Ideally, I would like one image at the start to set the scene for the story and one towards the end of the story concluding it",
-                "Always bring the story to a meaningful closure and end with 'THE END'"
-            ],
-            prompt=prompt,
-            response_format=cls
-        )
-
-        def audio_generator():
-            return ask_ai(
-                mode="tts",
-                instructions=[
-                    "Female, 30s, friendly, motherly, soft, slow, positive",
-                    f"reading a {'soothing bedtime' if bedtime else 'exciting'} story to a {who}",
-                    "speak slowly as if lulling a child to sleep" if bedtime else "show excitement in your voice; try to engage the child",
-                    "Easy and clear pronunciation for a child to understand"
-                ],
-                prompt=re.sub(r"<.*?>", '', story.html),  # strip html tags
-                voice="coral",
-                speed=0.8 if bedtime else 0.9,  # slower speed for kids
-                response_format="b64_uri"
-            )
-
-        return story, audio_generator
-
+from models import Story
 
 app = FastAPI()
 
